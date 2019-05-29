f66604abcd908963c598542a674774f4fae3c2fe
unknown <cmzolczynski@gmail.com>
2019-05-29 09:33:25 -0700

    java update. still not working

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// var cpChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] 
+
+// var wins = 0;
+// var losses = 0;
+// var guessesLeft = 9;
+// var letterUser = [];
+// var eachofLetters = null;
+
+
