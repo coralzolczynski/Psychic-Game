f414b47fdea4f6f10b7983f10a7c19b48ab56400
unknown <cmzolczynski@gmail.com>
2019-05-30 10:41:58 -0700

    java update

--- LotR Word Guess/assets/javascript/game.js
+++ LotR Word Guess/assets/javascript/game.js
@@ -19,8 +19,75 @@ function startGame() {
     numBlanks = lettersInChosenWord.length
     blanksAndSuccesses = [];
     wrongGuesses = [];
-    
 
+    for (var i = 0; i < numBlanks; i++) {
+        blanksAndSuccesses.push("_");
+    }
 
-
+    document.getElementById("guesses-left").innerHTML = numGuesses;
+    document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 }
+
+function checkLetters(letter) {
+    var letterInWord = false;
+    
+    for (var i = 0; i < numBlanks; i++) {
+
+        if (chosenWord[i] === letter) {
+    
+          letterInWord = true;
+        }
+    }
+    
+    if (letterInWord) {
+
+        for (var j = 0; j < numBlanks; j++) {
+    
+          if (chosenWord[j] === letter) {
+    
+            blanksAndSuccesses[j] = letter;
+          }
+        }
+    
+        console.log(blanksAndSuccesses);
+    }
+    
+    else {
+        wrongGuesses.push(letter); 
+        numGuesses--;
+    }
+}
+
+function roundComplete() {
+
+    document.getElementById("guesses-left").innerHTML = numGuesses;
+    document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
+
+    if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
+        winCounter++;
+        alert("????");
+        document.getElementById("win-counter").innerHTML = winCounter;
+        startGame();
+    }
+
+    else if (numGuesses === 0) {
+        lossCounter++;
+        alert("????");
+        document.getElementById("loss-counter").innerHTML = lossCounter;
+        startGame();
+
+    }
+}    
+
+startGame();
+
+document.onkeyup = function(event) {
+
+    letterGuessed = String.fromCharCode(event.which).toLowerCase();
+  
+    checkLetters(letterGuessed);
+  
+    roundComplete();
+  };
